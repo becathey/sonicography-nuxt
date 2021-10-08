01508f4c57570f03a474104294e4ea1029ea58b5
Bill Cathey <billcathey@hotmail.com>
2021-10-08 18:15:10 -0400

    Create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div><nuxt /></div>
+</template>
+
+<style>
+.button-green {
+  display: inline-block;
+  border: 1px solid hsl(170, 40%, 40%);
+  border-radius: 4px;
+  margin: 5px;
+  padding: 10px 30px;
+  color: hsl(170, 40%, 40%);
+  text-decoration: none;
+}
+.button-green:hover {
+  color: #fff;
+  background-color: hsl(170, 40%, 40%);
+}
+</style>
