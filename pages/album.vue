7f39de3cf43e36b8e9e57190b8533313737e7b00
Bill Cathey <billcathey@hotmail.com>
2021-10-08 17:55:06 -0400

    Create basic album page

---
+++ pages/album.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">Confusion Is Sex</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+        dolorum maxime quidem rem officia nam assumenda consequatur facere porro
+        illo, doloribus sed adipisci voluptates consectetur saepe voluptas qui
+        quibusdam molestias!
+      </p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  line-height: 1.5;
+}
+.article * {
+  margin-bottom: 1rem;
+}
+.title {
+  font-size: 2rem;
+}
+</style>
