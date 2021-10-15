e951a0e0a167bfd910695424b319342152820a4b
Bill Cathey <billcathey@hotmail.com>
2021-10-14 22:55:54 -0400

    Add album descriptions to data

--- store/albums.js
+++ store/albums.js
@@ -46,7 +46,7 @@ export const state = () => ({
       year: 1990,
       image: "/album-covers/goo.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        "Goo is the sixth studio album by Sonic Youth, released on June 26, 1990 by DGC Records. For this album, the band sought to expand upon its trademark alternating guitar arrangements and the layered sound of their previous album Daydream Nation (1988). The band's songwriting on Goo is more topical than past works, exploring themes of female empowerment and pop culture. Goo has been viewed as one of alternative rock's most important albums, and is considered musically and artistically significant. In 2020, the album was ranked at number 358 on Rolling Stone's 500 greatest albums of all time list. Goo expanded upon the alt-rock stylings of Daydream Nation with far more deliberate pop culture references. The album's front cover design was created by Raymond Pettibon, who was responsible for early covers for Black Flag. Instead of his original Joan Crawford sketch, Sonic Youth chose another Pettibon design: an illustration of two sunglasses-wearing British mods, based on a photograph of Maureen Hindley and David Smith, two witnesses in the Moors murders trial."
     },
     {
       id: "dirty",
@@ -54,7 +54,7 @@ export const state = () => ({
       year: 1992,
       image: "/album-covers/dirty.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        'Dirty is the seventh studio album by Sonic Youth, released on July 21, 1992 by DGC Records. The band recorded and co-produced the album with Butch Vig in early 1992 at the Magic Shop studios. The sound on Dirty was inspired by the grunge scene of the time, and was described as avant-rock. Some songs on the album mark the first appearance of three guitars in Sonic Youth songs. Dirty reached number 83 on the US Billboard 200 and number six on the UK Albums Chart. In support of the album, Sonic Youth embarked on the "Pretty Fucking Dirty" tour of 1992 and 1993, where they played most of the album during sets. In late 1992, they toured North America, and in early 1993, they toured New Zealand and Australia and released the Whores Moaning EP, which featured most of the "Sugar Kane" B-sides. The front cover of the album, a photograph taken by Gordon\'s friend Mike Kelley, depicted an orange stuffed toy with Sonic Youth written down the sides.'
     },
     {
       id: "jetSet",
@@ -62,7 +62,7 @@ export const state = () => ({
       year: 1994,
       image: "/album-covers/jet-set.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        "Experimental Jet Set, Trash and No Star is the eighth studio album by Sonic Youth, released on May 10, 1994 by DGC Records. It was produced by Butch Vig and recorded at Sear Sound studio in New York City, the same studio where the band's 1987 album Sister was recorded. Unlike its predecessor Dirty, Experimental Jet Set features a more low-key approach and references the band's earlier work on the independent record label SST Records. The album contains quieter and more relaxed songs that deal with personal and political topics. Upon release, Experimental Jet Set reached No. 34 on the US Billboard Top 200 chart and No. 10 on the UK Albums Chart. It was the band's highest peak on the US charts until their 2009 album The Eternal reached No. 18. The song \"Bull in the Heather\" was released as a single and as a music video featuring Kathleen Hanna of Bikini Kill. The album received generally favorable reviews from music critics, who highlighted the band's ability to create both noise and melody."
     },
     {
       id: "washingMachine",
@@ -70,7 +70,7 @@ export const state = () => ({
       year: 1995,
       image: "/album-covers/washing-machine.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        "Washing Machine is the ninth studio album by Sonic Youth, released on September 26, 1995 by DGC Records. It was recorded at Easley Studios in Memphis, Tennessee and produced by the band and John Siket, who also engineered the band's previous two albums. The album features more open-ended pieces than its predecessors and contains some of the band's longest songs, including the 20-minute ballad \"The Diamond Sea\", which is the lengthiest track to feature on any of Sonic Youth's studio albums. Released shortly after the band concluded their stint headlining the 1995 Lollapalooza music festival, Washing Machine reached No. 58 on the US Billboard 200 chart and No. 39 on the UK Albums Chart. The album received generally positive reviews from music critics, who praised the band for exploring new challenges as well as the guitar playing of band members Thurston Moore and Lee Ranaldo. It was ranked No. 18 in The Village Voice's 1995 Pazz & Jop critics' poll. Washing Machine is the band's first album on which Gordon almost exclusively played guitar instead of bass, resulting in a three-guitar and drums lineup. The album cover consists of a cropped Polaroid photograph of two unidentified fans taken at a Sonic Youth show in Amherst, Massachusetts in April 1995, during a short tour undertaken while the album was still in production. The fans are depicted wearing T-shirts that were sold as merchandise during that tour; early in 1995, the band was toying with the idea of changing their name to Washing Machine. Visible on the shirt on the left are signatures by Thalia Zedek and Chris Brokaw of the tour's opening band Come. The photo was taken by Gordon, who believed it could be used as the album cover. The band liked the shot, but the record label did not want to use it without permission from the fans. Because the band did not have any way to contact them, their faces had to be cropped out."
     },
     {
       id: "leaves",
@@ -78,7 +78,7 @@ export const state = () => ({
       year: 1998,
       image: "/album-covers/thousand-leaves.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        'A Thousand Leaves is the tenth studio album by Sonic Youth, released on May 12, 1998 by DGC Records. It was the band\'s first album recorded at their own studio in Lower Manhattan, which was built with the money they had made at the 1995 Lollapalooza festival. Since the band had an unlimited amount of time to work in their studio, the album features numerous lengthy and improvisational tracks that were developed unevenly. The highly experimental extended plays Anagrama, Slaapkamers met slagroom, and Invito al ĉielo were recorded simultaneously with the album. A Thousand Leaves reached number 85 on the US Billboard 200 and number 38 on the UK Albums Chart. It received generally favorable reviews from critics, who praised the lengthy, quiet guitar interplay between band members Thurston Moore and Lee Ranaldo. However, some criticized the forced vocal delivery of member Kim Gordon and found several tracks to be unnecessarily long and poorly constructed. The song "Sunday" was released as a single with a music video directed by Harmony Korine and starring American actor Macaulay Culkin. Musically, A Thousand Leaves was considered more expansive and relaxed than previous Sonic Youth albums, with less feedback and more guitar playing and improvisation. The album cover features an artwork by artist Marnie Weber, which depicts a hamster and Weber herself at age 12 sporting animal horns. According to Moore, the image is a reference to the Unlimited Edition compilation album by German experimental rock band Can. Originally, the album was titled Mille Feuille (French for A Thousand Leaves) and was intended to feature an image of Moore holding a pastry as the cover art.'
     },
     {
       id: "ghosts",
@@ -86,7 +86,7 @@ export const state = () => ({
       year: 2000,
       image: "/album-covers/ghosts-flowers.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        'NYC Ghosts & Flowers is the eleventh studio album by Sonic Youth, released on May 16, 2000 by DGC Records. The highly experimental album is considered to be a reaction to the theft of the band\'s instruments in July 1999, when several irreplaceable guitars and effects pedals were stolen. NYC Ghosts & Flowers was the first album since Bad Moon Rising in which the band used prepared guitar. As a result of the theft, the members of Sonic Youth relied upon "old guitars in their studio, unearthing instruments they hadn\'t used in years" which "along with equipment purchased to fulfill the remaining [...] dates [of the tour], would serve as the foundation for six new songs written over the next month", in addition to "Free City Rhymes" and "Renegade Princess", which were written prior to the tour. The band members later acknowledged that "the gear theft was somewhat of a blessing, if [also] a rather unwelcome and unpleasant one, in that it truly forced them to \'start over\' and approach creating music with brand new boundaries". On this album, the influence of beat poetry on the band was strongly evident: The lyrics to most songs resembled the beat style; Lenny Bruce and D. A. Levy were name-checked; and the cover art was based on a painting by William S. Burroughs.'
     },
     {
       id: "murray",
@@ -94,7 +94,7 @@ export const state = () => ({
       year: 2002,
       image: "/album-covers/murray-street.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        "Murray Street (sometimes written as Murray St.) is the twelfth studio album by American rock band Sonic Youth, released on June 25, 2002 by DGC Records. Murray Street is the first album by the band to feature Jim O'Rourke as an official fifth member to bolster the group's sound. Art director Frank Olinsky led the conception of the cover designs. The young blonde-haired girl pictured to the right on the album's cover is Coco Hayley Moore, Thurston Moore and Kim Gordon's daughter, picking strawberries with her friend, Stella, who is on the left. No title is shown on the cover, just a scrawled \"SONIC YOUTH\"."
     },
     {
       id: "nurse",
@@ -102,7 +102,7 @@ export const state = () => ({
       year: 2004,
       image: "/album-covers/sonic-nurse.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        'Sonic Nurse is the thirteenth studio album by Sonic Youth, released on June 8, 2004 by Geffen Records. The album\'s cover art was designed by artist Richard Prince from his Nurse Paintings series. Furthermore, one of Prince\'s photographic creations in this series was titled "Dude Ranch Nurse", which is also the name of a song on this record. "Pattern Recognition" was based on the 2003 William Gibson novel of the same name. Sonic Youth had used Gibson\'s work as an influence before, notably on a few tracks from Daydream Nation (1988). "Kim Gordon and the Arthur Doyle Hand Cream" was previously released as "Mariah Carey and the Arthur Doyle Hand Cream" on the first Narnack Buddy Series 7", but the title was changed due to controversy.'
     },
     {
       id: "ripped",
@@ -110,7 +110,7 @@ export const state = () => ({
       year: 2006,
       image: "/album-covers/rather-ripped.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        "Rather Ripped is the fourteenth studio album by Sonic Youth, released on June 13, 2006 by Geffen Records. It was the band's first album following the departure of multi-instrumentalist Jim O'Rourke, who had joined as a fifth member in 1999. Unlike its immediate predecessors, the album was produced by John Agnello and recorded at Sear Sound in New York City, the same studio where the band's 1994 album, Experimental Jet Set, Trash and No Star, was recorded. It also completed Sonic Youth's contract with Geffen, which released the band's previous eight records. Upon its release, the album peaked at number 71 on the US Billboard 200 and number 64 on the UK Albums Chart. It received mostly positive reviews from critics, who praised its simpler, cleaner melodies and the vocal delivery of singer and bassist Kim Gordon. The album was ranked number 12 in The Village Voice's 2006 Pazz & Jop critics' poll. Its only single, \"Incinerate\", was released in 2006, alongside an accompanying music video directed by French director and writer Claire Denis. Partially inspired by the streamlined approach taken by Blondie for their 1978 commercial breakthrough Parallel Lines, Moore decided to write simpler songs \"for everybody to plug into immediately.\" While he conceded Parallel Lines features some \"super good\" songs, he was disappointed with the album's poppier style and Mike Chapman's production when it was released. Nevertheless, he recognized that Parallel Lines became Blondie's breakthrough with the general public, and aimed to make Rather Ripped into Sonic Youth's version of Parallel Lines. The album's working titles were \"Sonic Life\" and \"Do You Believe in Rapture?\". The name \"Rather Ripped\" came from a Berkeley, California record store that later moved to Pittsburgh, Pennsylvania. Rather Ripped is generally considered one of the band's most accessible albums, featuring an abundance of concise and catchy melodies. The album generally favors guitar textures over feedback or noise, which typically characterized the band's earlier works."
     },
     {
       id: "eternal",
@@ -118,7 +118,7 @@ export const state = () => ({
       year: 2009,
       image: "/album-covers/eternal.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        "The Eternal is the fifteenth and final studio album by American alternative rock band Sonic Youth, released on June 9, 2009 by Matador Records, their first and only on that label. It was their first studio album in three years (since Rather Ripped), making it the band's longest delay between studio albums. The Eternal was the band's highest charting album of their career in the United States, peaking at No. 18 on the Billboard 200. The cover art was painted by John Fahey."
     }
   ]
 });
