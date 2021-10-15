36573a79af1368adbff368d210b966ca5117a6fc
Bill Cathey <billcathey@hotmail.com>
2021-10-14 22:03:21 -0400

    Add album descriptions to data

--- store/albums.js
+++ store/albums.js
@@ -6,7 +6,7 @@ export const state = () => ({
       year: 1983,
       image: "/album-covers/confusion.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        'Confusion Is Sex is the debut studio album by Sonic Youth. It was released on vinyl LP in February 1983 by record label Neutral. It has been cited as an important example of the no wave genre. It was mostly recorded during Jim Sclavunos\' brief tenure as drummer for the band, and he appears on drums for most of the album. As the sessions drew to a close, Sclavunos quit the band and Bob Bert was invited back. Bert appeared on "Making the Nature Scene" and the live Stooges cover "I Wanna Be Your Dog". Confusion Is Sex is the only Sonic Youth album on which guitarist Lee Ranaldo plays bass (on "Protect Me You”). Ranaldo recorded the track "Lee Is Free" solo at home on two tape recorders. The cover image is a sketch by bassist Kim Gordon of guitarist Thurston Moore. All lyrics are written by Sonic Youth (Kim Gordon, Thurston Moore, Lee Ranaldo, Jim Sclavunos and Bob Bert), except "The World Looks Red", by Michael Gira; all music is composed by Sonic Youth, except "I Wanna Be Your Dog", by the Stooges.'
     },
     {
       id: "badMoon",
@@ -14,7 +14,7 @@ export const state = () => ({
       year: 1985,
       image: "/album-covers/bad-moon.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        "Bad Moon Rising is the second studio album by Sonic Youth, released in March 1985 by Blast First and Homestead Records. The album is loosely themed around the dark side of America, including references to obsession, insanity, Charles Manson, heavy metal, Satanism, and early European settlers' encounters with Native Americans. Bad Moon Rising was the band's first album to combine experimental material with transitional pieces and segues. The album was named after the 1969 song Bad Moon Rising by Creedence Clearwater Revival. The album's cover is a photograph by artist James Welling of a scarecrow with a flaming jack-o'-lantern as its head. Bad Moon Rising's style has been described as noise rock, no wave, and experimental rock. Sonic Youth's use of transitional pieces in the album was inspired by their live shows, which featured either Moore or Ranaldo tuning guitars for up to five minutes while the other played slow transitory guitar riffs or prerecorded sound collages."
     },
     {
       id: "evol",
@@ -22,7 +22,7 @@ export const state = () => ({
       year: 1986,
       image: "/album-covers/evol.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        "EVOL is the third studio album by Sonic Youth. It was released in May 1986 by SST Records, the band's first release on the label. The album is notable for being the first with new drummer Steve Shelley (replacing Bob Bert), and for showing signs of the band's transition from their no wave past toward a greater pop sensibility. On the vinyl version of the album, the time length for \"Expressway to Yr. Skull\" was indicated by the infinity symbol (∞); the final moment of the song featured a locked groove that repeatedly plays the last few seconds of a song, making it theoretically endless. The album cover features a picture of model/actress Lung Leg, a still taken from Submit to Me."
     },
     {
       id: "sister",
@@ -30,7 +30,7 @@ export const state = () => ({
       year: 1987,
       image: "/album-covers/sister.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        'Sister is the fourth studio album by Sonic Youth. It was released in June 1987 by SST Records. The album furthered the band\'s move away from the no wave genre towards more traditional song structures, while maintaining an aggressively experimental approach. Sister was recorded to 16-track in March and April 1987 with Walter Sear at Sear Sound, entirely on analog tube equipment, giving it its characteristic "warm", vintage feel. Sister is a loose concept album (like its follow-up Daydream Nation). Sister was in part inspired by the life and works of science fiction writer Philip K. Dick. It was named Sister as a reference to Dick\'s fraternal twin, who died shortly after her birth, and whose memory haunted Dick his entire life. The album features aggressive noise songs such as "White Kross" and "Catholic Block", as well as a menacing noir ode, "Pacific Coast Highway", although it also featured more traditional song structures. "Kotton Krown" (or "Cotton Crown") was the first Gordon and Moore duet, although Moore usually sang it alone during live performances. The artwork of the original front cover contained a photograph of 12-year-old Sandra Bennett, taken by Richard Avedon on August 23, 1980, but it was censored for later releases after a threat of a lawsuit. At first the picture was merely covered up with a black sticker, but on later pressings it was removed, only showing a black area.'
     },
     {
       id: "daydream",
@@ -38,7 +38,7 @@ export const state = () => ({
       year: 1988,
       image: "/album-covers/daydream-nation.png",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem qui, asperiores architecto, pariatur consequuntur dicta hic dolorum consectetur laudantium iusto placeat eaque cumque, odit saepe! Voluptates perspiciatis ullam consequatur architecto esse, eligendi voluptatum obcaecati eius temporibus velit vero earum ea. Esse expedita illum dolor magni, accusantium ipsam. Velit, esse ex!"
+        "Daydream Nation is the fifth studio album by Sonic Youth, released on October 18, 1988. The band recorded the album between July and August 1988 at Greene St. Recording in New York City, and it was released by Enigma Records as a double album. After Daydream Nation was released, it received widespread acclaim from critics and earned Sonic Youth a major label deal. The album was ranked high in critics' year-end lists of 1988's best records, being voted second in The Village Voice's annual Pazz & Jop poll. Daydream Nation has since been widely considered to be Sonic Youth's greatest work, as well as one of the greatest albums of all time, specifically having a profound influence on the alternative and indie rock genres. It was chosen by the Library of Congress to be preserved in the National Recording Registry in 2005. Daydream Nation is generally considered an avant-rock, alternative rock, indie rock, art punk, and post-punk album, with the record being notable for its unorthodox guitar tuning and song structure, with many songs concluding with lengthy instrumental sections. The cover for Daydream Nation features the 1983 Gerhard Richter painting Kerze (\"Candle\")."
     },
     {
       id: "goo",
