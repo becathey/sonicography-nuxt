83d43e50643293917789d59392d257506d8ef789
Bill Cathey <billcathey@hotmail.com>
2021-10-15 23:35:42 -0400

    Add styles to text on The Band page

--- pages/band.vue
+++ pages/band.vue
@@ -5,141 +5,145 @@
       <div class="band-member">
         <img src="/band-members/moore.jpg" class="musician-portrait" />
         <p>
-          Thurston Moore (born July 25, 1958) is an American musician best known
-          as a member of Sonic Youth. He has also participated in many solo and
-          group collaborations outside Sonic Youth, as well as running the
-          Ecstatic Peace! record label. Moore was ranked 34th in Rolling Stone's
-          2004 edition of the "100 Greatest Guitarists of All Time." In May
-          2012, Spin published a staff-selected list of the top 100 rock
-          guitarists, and ranked Moore and his Sonic Youth bandmate Lee Ranaldo
-          together at number 1. In 2012, Moore started a new band Chelsea Light
-          Moving, whose eponymous debut was released on March 5, 2013. Since
-          2015, Chelsea Light Moving has been disbanded after 1 studio album
-          release. Moore and the other members of the band continue to make
-          music under his solo project and other bands.
+          <span class="musician-name">Thurston Moore</span> (born July 25, 1958)
+          is an American musician best known as a member of Sonic Youth. He has
+          also participated in many solo and group collaborations outside Sonic
+          Youth, as well as running the Ecstatic Peace! record label. Moore was
+          ranked 34th in <i>Rolling Stone</i>'s 2004 edition of the "100
+          Greatest Guitarists of All Time." In May 2012, <i>Spin</i> published a
+          staff-selected list of the top 100 rock guitarists, and ranked Moore
+          and his Sonic Youth bandmate Lee Ranaldo together at number 1. In
+          2012, Moore started a new band Chelsea Light Moving, whose eponymous
+          debut was released on March 5, 2013. Since 2015, Chelsea Light Moving
+          has been disbanded after 1 studio album release. Moore and the other
+          members of the band continue to make music under his solo project and
+          other bands.
         </p>
       </div>
       <div class="band-member">
         <img src="/band-members/gordon.jpg" class="musician-portrait" />
         <p>
-          Kim Gordon (born April 28, 1953) is an American musician, singer and
-          songwriter, best known as the bassist, guitarist, and vocalist of
-          alternative rock band Sonic Youth. Born in Rochester, New York, she
-          was raised in Los Angeles, California, where her father was a
-          professor at the University of California, Los Angeles. After
-          graduating from Los Angeles's Otis College of Art and Design, she
-          moved to New York City to begin an art career. There, she formed Sonic
-          Youth with Thurston Moore in 1981. She and Moore married in 1984.
-          Gordon was also a founding member of the musical project Free Kitten,
-          which she formed with Julia Cafritz in 1993. Following the dissolution
-          of Sonic Youth and her divorce from Moore, Gordon formed the
-          experimental duo Body/Head with Bill Nace, releasing their debut album
-          Coming Apart in 2013. She subsequently formed Glitterbust with Alex
-          Knost, releasing a self-titled debut album in 2016. Body/Head released
-          their second studio album, The Switch, in 2018. She released her first
-          solo album, No Home Record, in 2019. In addition to her work as a
-          musician, Gordon has had ventures in record producing, fashion, and
-          acting, and has worked consistently as a visual artist throughout her
-          musical career. She debuted as a producer on Hole's debut album Pretty
-          on the Inside (1991), and founded the Los Angeles-based clothing line
-          X-Girl in 1993. Beginning in the mid-2000s, Gordon began acting,
-          making minor appearances in such films as Last Days (2005) and I'm Not
-          There (2007), followed by guest-starring appearances on several
-          television series. In February 2015, she published a memoir, Girl in a
-          Band, by HarperCollins imprint Dey Street Books.
+          <span class="musician-name">Kim Gordon</span> (born April 28, 1953) is
+          an American musician, singer and songwriter, best known as the
+          bassist, guitarist, and vocalist of alternative rock band Sonic Youth.
+          Born in Rochester, New York, she was raised in Los Angeles,
+          California, where her father was a professor at the University of
+          California, Los Angeles. After graduating from Los Angeles's Otis
+          College of Art and Design, she moved to New York City to begin an art
+          career. There, she formed Sonic Youth with Thurston Moore in 1981. She
+          and Moore married in 1984. Gordon was also a founding member of the
+          musical project Free Kitten, which she formed with Julia Cafritz in
+          1993. Following the dissolution of Sonic Youth and her divorce from
+          Moore, Gordon formed the experimental duo Body/Head with Bill Nace,
+          releasing their debut album Coming Apart in 2013. She subsequently
+          formed Glitterbust with Alex Knost, releasing a self-titled debut
+          album in 2016. Body/Head released their second studio album, The
+          Switch, in 2018. She released her first solo album, No Home Record, in
+          2019. In addition to her work as a musician, Gordon has had ventures
+          in record producing, fashion, and acting, and has worked consistently
+          as a visual artist throughout her musical career. She debuted as a
+          producer on Hole's debut album Pretty on the Inside (1991), and
+          founded the Los Angeles-based clothing line X-Girl in 1993. Beginning
+          in the mid-2000s, Gordon began acting, making minor appearances in
+          such films as <i>Last Days</i> (2005) and <i>I'm Not There</i> (2007),
+          followed by guest-starring appearances on several television series.
+          In February 2015, she published a memoir, <i>Girl in a Band</i>, by
+          HarperCollins imprint Dey Street Books.
         </p>
       </div>
       <div class="band-member">
         <img src="/band-members/ranaldo.jpg" class="musician-portrait" />
         <p>
-          Lee Ranaldo (born February 3, 1956) is an American musician,
-          singer-songwriter, guitarist, writer, visual artist and record
-          producer, best known as a co-founder of the alternative rock band
-          Sonic Youth (guitar and vocals). In 2004, Rolling Stone ranked Ranaldo
-          at number 33 on its "Greatest Guitarists of All Time" list. In May
-          2012, Spin published a staff-selected top 100 guitarist list, ranking
-          Ranaldo and his Sonic Youth bandmate Thurston Moore together at number
-          1. Ranaldo was born in Glen Cove, Long Island, studied art and
-          graduated from Binghamton University. He has three sons, Cody, Sage
-          and Frey, and is married twice, first with Amanda Linn in 1981 but
-          later divorced, and now with experimental artist Leah Singer. Ranaldo
-          started his career in New York in several bands, and joined the
-          electric guitar orchestra of Glenn Branca. In this orchestra he played
-          mainly electric guitar, but also some of Branca's harmonic guitars
-          Branca had designed and built himself. In 1981, he and David Linton
-          briefly joined the band Plus Instruments formed by Truus de Groot.
-          With this line-up they recorded the album February - April 1981,
-          released on the Dutch Kremlin label. After the release of the album,
-          Ranaldo left the band and started Sonic Youth with Thurston Moore and
-          Kim Gordon.
+          <span class="musician-name">Lee Ranaldo</span> (born February 3, 1956)
+          is an American musician, singer-songwriter, guitarist, writer, visual
+          artist and record producer, best known as a co-founder of the
+          alternative rock band Sonic Youth (guitar and vocals). In 2004,
+          <i>Rolling Stone</i> ranked Ranaldo at number 33 on its "Greatest
+          Guitarists of All Time" list. In May 2012, <i>Spin</i> published a
+          staff-selected top 100 guitarist list, ranking Ranaldo and his Sonic
+          Youth bandmate Thurston Moore together at number 1. Ranaldo was born
+          in Glen Cove, Long Island, studied art and graduated from Binghamton
+          University. He has three sons, Cody, Sage and Frey, and is married
+          twice, first with Amanda Linn in 1981 but later divorced, and now with
+          experimental artist Leah Singer. Ranaldo started his career in New
+          York in several bands, and joined the electric guitar orchestra of
+          Glenn Branca. In this orchestra he played mainly electric guitar, but
+          also some of Branca's harmonic guitars Branca had designed and built
+          himself. In 1981, he and David Linton briefly joined the band Plus
+          Instruments formed by Truus de Groot. With this line-up they recorded
+          the album February - April 1981, released on the Dutch Kremlin label.
+          After the release of the album, Ranaldo left the band and started
+          Sonic Youth with Thurston Moore and Kim Gordon.
         </p>
       </div>
       <div class="band-member">
         <img src="/band-members/shelley.jpg" class="musician-portrait" />
         <p>
-          Steven Shelley (born June 23, 1962) is an American drummer, best known
-          as a member of the alternative rock band Sonic Youth. Shelley was born
-          in Midland, Michigan and played in several mid-Michigan bands,
-          including Faith and Morals and Strange Fruit. He moved to Manhattan
-          with a friend, living in the apartment of Sonic Youth's Thurston Moore
-          and Kim Gordon for dog sitting purposes while the band was in Europe.
-          When the band returned, former drummer Bob Bert had left the band, and
-          they hired Shelley as their drummer without an audition. In 1992 he
-          founded the independent record label Smells Like Records, based in
-          Hoboken, New Jersey. In 1998 Shelley played on the soundtrack of the
-          film Velvet Goldmine as a member of Wylde Ratttz, along with The
-          Stooges' Ron Asheton, Thurston Moore, Mike Watt, Don Fleming, Mark Arm
-          of Mudhoney and Jim Dunbar. Other significant work away from Sonic
-          Youth includes recording and performing with an array of artists such
-          as Christina Rosenvinge, Chris Lee, John Wolfington, Michael Powers,
-          Michael Rother, Howe Gelb, and Enrique Morente. His enthusiasm for the
-          music of Daniel Johnston and Townes Van Zandt led to his appearances
-          in the documentaries The Devil and Daniel Johnston and Be Here to Love
-          Me. Shelley is also in charge of operating Sonic Youth's own record
+          <span class="musician-name">Steven Shelley</span> (born June 23, 1962)
+          is an American drummer, best known as a member of the alternative rock
+          band Sonic Youth. Shelley was born in Midland, Michigan and played in
+          several mid-Michigan bands, including Faith and Morals and Strange
+          Fruit. He moved to Manhattan with a friend, living in the apartment of
+          Sonic Youth's Thurston Moore and Kim Gordon for dog sitting purposes
+          while the band was in Europe. When the band returned, former drummer
+          Bob Bert had left the band, and they hired Shelley as their drummer
+          without an audition. In 1992 he founded the independent record label
+          Smells Like Records, based in Hoboken, New Jersey. In 1998 Shelley
+          played on the soundtrack of the film <i>Velvet Goldmine</i> as a
+          member of Wylde Ratttz, along with The Stooges' Ron Asheton, Thurston
+          Moore, Mike Watt, Don Fleming, Mark Arm of Mudhoney and Jim Dunbar.
+          Other significant work away from Sonic Youth includes recording and
+          performing with an array of artists such as Christina Rosenvinge,
+          Chris Lee, John Wolfington, Michael Powers, Michael Rother, Howe Gelb,
+          and Enrique Morente. His enthusiasm for the music of Daniel Johnston
+          and Townes Van Zandt led to his appearances in the documentaries
+          <i>The Devil and Daniel Johnston</i> and <i>Be Here to Love Me</i>.
+          Shelley is also in charge of operating Sonic Youth's own record
           labels, SYR and Goofin'.
         </p>
       </div>
       <div class="band-member">
         <img src="/band-members/orourke.jpg" class="musician-portrait" />
         <p>
-          Jim O'Rourke (born January 18, 1969, Chicago, Illinois) is a
-          Tokyo-based American musician, composer and record producer. He has
-          released albums across varied genres, including singer-songwriter
-          music, post-rock, ambient, noise music, and tape experiments. O’Rourke
-          is best known for his numerous solo and collaborative music projects,
-          many of which are entirely instrumental, and for his tenure as a
-          member of Sonic Youth from 1999 to 2005. O'Rourke has collaborated
-          with Thurston Moore, Lee Ranaldo, Kim Gordon, Steve Shelley, Derek
-          Bailey, Mats Gustafsson, Mayo Thompson, Brigitte Fontaine, Loren
-          Mazzacane Connors, Merzbow, Nurse with Wound, Phill Niblock, Fennesz,
-          Organum, Phew, Henry Kaiser, Flying Saucer Attack. He has produced
-          albums by artists such as Sonic Youth, Wilco, Stereolab, Superchunk,
-          Kahimi Karie, Quruli, John Fahey, Smog, Faust, Tony Conrad, The Red
-          Krayola, Bobby Conn, Beth Orton, Joanna Newsom and U.S. Maple. He
-          mixed Wilco's Yankee Hotel Foxtrot album and produced their 2004
-          album, A Ghost Is Born, for which he won a Grammy Award for "Best
-          Alternative Album". Beginning in 1999 he played bass guitar, guitar
-          and synthesizer with Sonic Youth, in addition to recording and mixing
-          duties with the group. He withdrew as a full member in late 2005, but
-          continued to play with them in some of their side projects.
+          <span class="musician-name">Jim O'Rourke</span> (born January 18,
+          1969, Chicago, Illinois) is a Tokyo-based American musician, composer
+          and record producer. He has released albums across varied genres,
+          including singer-songwriter music, post-rock, ambient, noise music,
+          and tape experiments. O’Rourke is best known for his numerous solo and
+          collaborative music projects, many of which are entirely instrumental,
+          and for his tenure as a member of Sonic Youth from 1999 to 2005.
+          O'Rourke has collaborated with Thurston Moore, Lee Ranaldo, Kim
+          Gordon, Steve Shelley, Derek Bailey, Mats Gustafsson, Mayo Thompson,
+          Brigitte Fontaine, Loren Mazzacane Connors, Merzbow, Nurse with Wound,
+          Phill Niblock, Fennesz, Organum, Phew, Henry Kaiser, and Flying Saucer
+          Attack. He has produced albums by artists such as Sonic Youth, Wilco,
+          Stereolab, Superchunk, Kahimi Karie, Quruli, John Fahey, Smog, Faust,
+          Tony Conrad, The Red Krayola, Bobby Conn, Beth Orton, Joanna Newsom
+          and U.S. Maple. He mixed Wilco's Yankee Hotel Foxtrot album and
+          produced their 2004 album, A Ghost Is Born, for which he won a Grammy
+          Award for "Best Alternative Album". Beginning in 1999 he played bass
+          guitar, guitar and synthesizer with Sonic Youth, in addition to
+          recording and mixing duties with the group. He withdrew as a full
+          member in late 2005, but continued to play with them in some of their
+          side projects.
         </p>
       </div>
       <div class="band-member">
         <img src="/band-members/ibold.jpg" class="musician-portrait" />
         <p>
-          Mark Ibold (born October 17, 1962) is an American bass guitarist who
-          was a member of the indie band Pavement from 1992 to 1999, and again
-          for their 2010 reunion. He was also in the alternative rock band Sonic
-          Youth from 2006 until their 2011 breakup, mainly on bass but
-          occasionally playing guitar. He is based in New York. Prior to joining
-          Pavement, he was a member of the Dustdevils in the late 1980s and
-          early 1990s. While a member of Pavement, Ibold joined Free Kitten
-          along with Sonic Youth's Kim Gordon, Pussy Galore's Julie Cafritz and
-          Boredoms' Yoshimi P-We. In 2006, Ibold joined Sonic Youth on their
-          Rather Ripped tour, and he also appeared with the band on their first
-          encores during the 2007 "Don't Look Back" shows. As of their 2009
-          album The Eternal, he was a full participating member of the band
-          until they split up in 2011.
+          <span class="musician-name">Mark Ibold</span> (born October 17, 1962)
+          is an American bass guitarist who was a member of the indie band
+          Pavement from 1992 to 1999, and again for their 2010 reunion. He was
+          also in the alternative rock band Sonic Youth from 2006 until their
+          2011 breakup, mainly on bass but occasionally playing guitar. He is
+          based in New York. Prior to joining Pavement, he was a member of the
+          Dustdevils in the late 1980s and early 1990s. While a member of
+          Pavement, Ibold joined Free Kitten along with Sonic Youth's Kim
+          Gordon, Pussy Galore's Julie Cafritz and Boredoms' Yoshimi P-We. In
+          2006, Ibold joined Sonic Youth on their Rather Ripped tour, and he
+          also appeared with the band on their first encores during the 2007
+          "Don't Look Back" shows. As of their 2009 album The Eternal, he was a
+          full participating member of the band until they split up in 2011.
         </p>
       </div>
     </article>
@@ -163,5 +167,9 @@
   float: left;
   width: 250px;
   margin-right: 1rem;
+}
+.musician-name {
+  font-variant: small-caps;
+  font-weight: bold;
 }
 </style>
