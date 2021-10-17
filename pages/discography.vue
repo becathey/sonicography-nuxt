7df89d00803641d599ef9fcb35ab2827995ce5ee
Bill Cathey <billcathey@hotmail.com>
2021-10-16 23:53:21 -0400

    Add styles for Discography page

--- pages/discography.vue
+++ pages/discography.vue
@@ -5,31 +5,43 @@
       <p>
         A selected listing of Sonic Youth's studio albums and extended plays:
       </p>
-      <ul>
-        <li>1982 Sonic Youth (EP)</li>
-        <li>1983 Confusion Is Sex</li>
-        <li>1983 Kill Yr Idols (EP)</li>
-        <li>1985 Bad Moon Rising</li>
-        <li>1986 EVOL</li>
-        <li>1987 Sister</li>
-        <li>1987 Master=Dik (EP)</li>
-        <li>1988 Daydream Nation</li>
-        <li>1990 Goo</li>
-        <li>1992 Dirty</li>
-        <li>1993 Whores Moaning (EP)</li>
-        <li>1994 TV Shit (EP)</li>
-        <li>1994 Experimental Jet Set, Trash and No Star</li>
-        <li>1995 Washing Machine</li>
-        <li>1998 A Thousand Leaves</li>
-        <li>1998 Silver Session for Jason Knuth (EP)</li>
-        <li>2000 NYC Ghosts &amp; Flowers</li>
-        <li>2001 In the Fishtank 9 (EP)</li>
-        <li>2002 Kali Yug Express (EP)</li>
-        <li>2002 Murray Street</li>
-        <li>2004 Sonic Nurse</li>
-        <li>2006 Rather Ripped</li>
-        <li>2009 Sensational Fix (EP)</li>
-        <li>2009 The Eternal</li>
+      <ul class="discography">
+        <li><span class="discography-date">1982</span> Sonic Youth (EP)</li>
+        <li><span class="discography-date">1983</span> Confusion Is Sex</li>
+        <li><span class="discography-date">1983</span> Kill Yr Idols (EP)</li>
+        <li><span class="discography-date">1985</span> Bad Moon Rising</li>
+        <li><span class="discography-date">1986</span> EVOL</li>
+        <li><span class="discography-date">1987</span> Sister</li>
+        <li><span class="discography-date">1987</span> Master=Dik (EP)</li>
+        <li><span class="discography-date">1988</span> Daydream Nation</li>
+        <li><span class="discography-date">1990</span> Goo</li>
+        <li><span class="discography-date">1992</span> Dirty</li>
+        <li><span class="discography-date">1993</span> Whores Moaning (EP)</li>
+        <li><span class="discography-date">1994</span> TV Shit (EP)</li>
+        <li>
+          <span class="discography-date">1994</span> Experimental Jet Set, Trash
+          and No Star
+        </li>
+        <li><span class="discography-date">1995</span> Washing Machine</li>
+        <li><span class="discography-date">1998</span> A Thousand Leaves</li>
+        <li>
+          <span class="discography-date">1998</span> Silver Session for Jason
+          Knuth (EP)
+        </li>
+        <li>
+          <span class="discography-date">2000</span> NYC Ghosts &amp; Flowers
+        </li>
+        <li>
+          <span class="discography-date">2001</span> In the Fishtank 9 (EP)
+        </li>
+        <li>
+          <span class="discography-date">2002</span> Kali Yug Express (EP)
+        </li>
+        <li><span class="discography-date">2002</span> Murray Street</li>
+        <li><span class="discography-date">2004</span> Sonic Nurse</li>
+        <li><span class="discography-date">2006</span> Rather Ripped</li>
+        <li><span class="discography-date">2009</span> Sensational Fix (EP)</li>
+        <li><span class="discography-date">2009</span> The Eternal</li>
       </ul>
     </article>
   </div>
@@ -41,5 +53,14 @@
 }
 .title {
   font-size: 2rem;
+}
+.discography {
+  margin-top: 1rem;
+  list-style: none;
+}
+.discography-date {
+  margin-right: 0.75rem;
+  font-weight: bold;
+  color: var(--yellow);
 }
 </style>
