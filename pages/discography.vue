2007930b59b994b69ba813a686a94a39060bc262
Bill Cathey <billcathey@hotmail.com>
2021-10-16 23:43:06 -0400

    Add Discography page

---
+++ pages/discography.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">Discography</h1>
+      <p>
+        A selected listing of Sonic Youth's studio albums and extended plays:
+      </p>
+      <ul>
+        <li>1982 Sonic Youth (EP)</li>
+        <li>1983 Confusion Is Sex</li>
+        <li>1983 Kill Yr Idols (EP)</li>
+        <li>1985 Bad Moon Rising</li>
+        <li>1986 EVOL</li>
+        <li>1987 Sister</li>
+        <li>1987 Master=Dik (EP)</li>
+        <li>1988 Daydream Nation</li>
+        <li>1990 Goo</li>
+        <li>1992 Dirty</li>
+        <li>1993 Whores Moaning (EP)</li>
+        <li>1994 TV Shit (EP)</li>
+        <li>1994 Experimental Jet Set, Trash and No Star</li>
+        <li>1995 Washing Machine</li>
+        <li>1998 A Thousand Leaves</li>
+        <li>1998 Silver Session for Jason Knuth (EP)</li>
+        <li>2000 NYC Ghosts &amp; Flowers</li>
+        <li>2001 In the Fishtank 9 (EP)</li>
+        <li>2002 Kali Yug Express (EP)</li>
+        <li>2002 Murray Street</li>
+        <li>2004 Sonic Nurse</li>
+        <li>2006 Rather Ripped</li>
+        <li>2009 Sensational Fix (EP)</li>
+        <li>2009 The Eternal</li>
+      </ul>
+    </article>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  line-height: 1.5;
+}
+.title {
+  font-size: 2rem;
+}
+</style>
